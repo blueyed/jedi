blueyed/jedi

commit 0352c3250ae9ad69dd1de78c37fc29895f3121ed

Fix signatures for __init__ calls when used with supers, fixes #1163

jedi/evaluate/context/klass.py
@@ -222,6 +222,10 @@ class ClassMixin(object):
                     next(instance_filters)
                     yield next(instance_filters)
 
+    def get_signatures(self):
+        init_funcs = self.execute_evaluated().py__getattribute__('__init__')
+        return [sig.bind(self) for sig in init_funcs.get_signatures()]
+
     def get_global_filter(self, until_position=None, origin_scope=None):
         return ParserTreeFilter(
             self.evaluator,
@@ -313,10 +317,6 @@ class ClassContext(use_metaclass(CachedMetaClass, ClassMixin, FunctionAndClassBa
                 generics=tuple(remap_type_vars())
             )])
         return ContextSet({self})
-
-    def get_signatures(self):
-        init_funcs = self.py__getattribute__('__init__')
-        return [sig.bind(self) for sig in init_funcs.get_signatures()]
 
     @plugin_manager.decorate()
     def get_metaclass_filters(self, metaclass):

test/test_evaluate/test_signature.py
@@ -108,9 +108,9 @@ def test_tree_signature(Script, environment, code, expected):
         ('full_redirect(1)', '*args, **kwargs'),
 
         # Classes / inheritance
-        ('full_redirect(C)', 'z, *c'),
+        ('full_redirect(C)', 'z, *, c'),
         ('full_redirect(C())', 'y'),
-        ('D', 'D(x, ly)'),
+        ('D', 'D(a, z, /)'),
         ('D()', 'D(x, y)'),
         ('D().foo', 'foo(a, *, bar, z, **kwargs)'),
 
@@ -166,14 +166,14 @@ def test_nested_signatures(Script, environment, combination, expected, skip_pre_
             return lambda *args, **kwargs: func1(1, 2, 3, 4, *args) + func2(a=3, x=1, y=1, **kwargs)
 
         class C:
-            def __init__(self, a, z, *c): ...
+            def __init__(self, a, z, *, c): ...
             def __call__(self, x, y): ...
 
             def foo(self, bar, z, **kwargs): ...
 
         class D(C):
             def __init__(self, *args):
-                super().foo(*args)
+                super().__init__(*args)
 
             def foo(self, a, **kwargs):
                 super().foo(**kwargs)

commit 97e323e7efd81ae56445b456452eacc982aa4745

Fix call signatures with `staticmethod()`
Regressed in 0352c325.

Fall back to the old method used before.

Fixes https://github.com/davidhalter/jedi-vim/pull/938#issuecomment-520266087

jedi/evaluate/context/klass.py
@@ -224,6 +224,8 @@ class ClassMixin(object):
 
     def get_signatures(self):
         init_funcs = self.execute_evaluated().py__getattribute__('__init__')
+        if not init_funcs:
+            init_funcs = self.py__getattribute__('__init__')
         return [sig.bind(self) for sig in init_funcs.get_signatures()]
 
     def get_global_filter(self, until_position=None, origin_scope=None):

test/test_evaluate/test_signature.py
@@ -186,14 +186,26 @@ def test_nested_signatures(Script, environment, combination, expected, skip_pre_
     assert expected == computed
 
 
-def test_pow_signature(Script):
-    # See github #1357
-    sigs = Script('pow(').call_signatures()
+@pytest.mark.parametrize(
+    'code, expected',
+    [
+        [
+            # See github #1357
+            'pow(',
+            {
+                'pow(x: float, y: float, z: float, /) -> float',
+                'pow(x: float, y: float, /) -> float',
+                'pow(x: int, y: int, z: int, /) -> Any',
+                'pow(x: int, y: int, /) -> Any',
+            },
+        ],
+        ['staticmethod(', {'staticmethod(f: Callable)'}],
+    ],
+)
+def test_basic_signatures(code, expected, Script):
+    sigs = Script(code).call_signatures()
     strings = {sig.to_string() for sig in sigs}
-    assert strings == {'pow(x: float, y: float, z: float, /) -> float',
-                       'pow(x: float, y: float, /) -> float',
-                       'pow(x: int, y: int, z: int, /) -> Any',
-                       'pow(x: int, y: int, /) -> Any'}
+    assert strings == expected
 
 
 @pytest.mark.parametrize(
@@ -224,6 +236,7 @@ def test_pow_signature(Script):
                 return wrapper
 
             x(f)('''), 'f()'],
+        ['staticmethod(', 'staticmethod(f: Callable)'],
     ]
 )
 def test_wraps_signature(Script, code, signature, skip_pre_python35):